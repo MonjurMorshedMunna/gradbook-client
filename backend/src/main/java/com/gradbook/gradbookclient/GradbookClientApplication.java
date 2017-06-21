a5fc97314aa0aba2182ced9d658d403c240e8216
Monjur Morshed <monjurmorshed794@outlook.com>
2017-06-21 21:52:27 +0600

    initial project configured

---
+++ backend/src/main/java/com/gradbook/gradbookclient/GradbookClientApplication.java
@@ -0,0 +1,12 @@
+package com.gradbook.gradbookclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradbookClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradbookClientApplication.class, args);
+	}
+}
